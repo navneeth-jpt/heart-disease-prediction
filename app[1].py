e3893c5bfad529b8a5173b4369d0ab8e606cd041
navneeth-jpt <2420080012@klh.edu.in>
2026-04-06 19:57:41 +0530

    Add files via upload

---
+++ app[1].py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load('model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['features']
+    prediction = model.predict([data])
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
