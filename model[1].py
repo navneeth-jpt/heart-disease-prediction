0e5830d84bb96c213a3673a18adc3702ac881bd7
navneeth-jpt <2420080012@klh.edu.in>
2026-04-06 19:58:41 +0530

    Add files via upload

---
+++ model[1].py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+# Load dataset
+df = pd.read_csv('heart.csv')
+
+X = df.drop('target', axis=1)
+y = df['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+joblib.dump(model, 'model.pkl')
+
+print("Model trained and saved!")
